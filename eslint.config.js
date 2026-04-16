c95808890e79f6d907136fd26fea4d09e6561fff
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-04-16 09:27:45 +0100

    chore: add configurations for eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,56 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import reactRefreshPlugin from "eslint-plugin-react-refresh";
+import prettier from "eslint-config-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
+import importPlugin from "eslint-plugin-import";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", ".vite", ".vite/**", "node_modules"]),
+
   {
     files: ["**/*.{js,jsx}"],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
       },
     },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    extends: [
+      js.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+      react.configs.flat.recommended,
+      reactRefreshPlugin.configs.vite,
+      reactHooks.configs.flat.recommended,
+      prettier, // MUST be last
+    ],
+
     rules: {
+      // React 17+ JSX runtime
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+
+      // Vite Fast Refresh safety
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "import/extensions": ["error", "always", { ignorePackages: true }],
+      "import/no-unresolved": "off",
     },
   },
 ]);
